73b8c5bb85e2746b749a00330877d99e926f163f
Shengyun Yin <z5098800@ad.unsw.edu.au>
2024-04-04 22:04:09 +1100

    Revert "regsiter and login"
    This reverts commit da6779c41dd2431081c1ec88feb05eec6e22dea9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
-import Dashboard from './components/Dashboard'; // 确保Dashboard.jsx的路径正确
-import Login from './components/Login'; // 确保Login.jsx的路径正确
-import Register from './components/Register'; // 确保Register.jsx的路径正确
 
-const App = () => {
+function App () {
   return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/regisddter">Register</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            {/* 这里可以添加更多路由 */}
-          </Routes>
-        </div>
-      </Router>
+    <>Let&apos;s go!</>
   );
-};
+}
 
 export default App;
