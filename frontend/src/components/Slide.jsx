59845545882100038329c2c1c544c4aa27739491
Shengyun Yin <z5098800@ad.unsw.edu.au>
2024-04-07 23:35:22 +1000

    resizable

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import ElementModal from './ElementModal';
+import ResizableBox from './ResizableBox';
 
 function Slide ({ slide, handleDeleteElement, handleUpdateElement }) {
   // State to control the modal for adding/editing elements
@@ -32,116 +31,20 @@ function Slide ({ slide, handleDeleteElement, handleUpdateElement }) {
 
   return (
     <div className="slide-container"
-         style={{ border: '2px solid black', margin: '20px', position: 'relative', width: '100%', height: '1000px' }}>
+         style={{ border: '2px solid black', margin: '20px', position: 'relative', width: '1600px', height: '1000px' }}>
       {slide.elements.map((element, index) => {
-        switch (element.type) {
-          case 'text':
-            return (
-              <div
-                key={index}
-                onContextMenu={(event) => {
-                  event.preventDefault();
-                  handleDeleteElement(element.id);
-                }}
-                onDoubleClick={(event) => {
-                  event.preventDefault();
-                  handleEditElement(element.id)
-                }}
-                style={{
-                  position: 'absolute',
-                  left: `${element.position.x}%`,
-                  top: `${element.position.y}%`,
-                  width: `${element.width}%`,
-                  height: `${element.height}%`,
-                  zIndex: index,
-                  border: '1px solid grey',
-                  color: element.color,
-                  fontSize: `${element.fontSize}em`,
-                }}
-              >
-                {element.text}
-              </div>
-            );
-          case 'image':
-            return (
-              <img
-                key={index}
-                onContextMenu={(event) => {
-                  event.preventDefault();
-                  handleDeleteElement(element.id);
-                }}
-                onDoubleClick={(event) => {
-                  event.preventDefault();
-                  handleEditElement(element.id)
-                }}
-                src={element.url}
-                alt={element.alt}
-                style={{
-                  position: 'absolute',
-                  left: `${element.position.x}%`,
-                  top: `${element.position.y}%`,
-                  width: `${element.width}%`,
-                  height: `${element.height}%`,
-                  zIndex: index,
-                }}
-              />
-            );
-          case 'video':
-            return (
-              <video
-                key={index}
-                onContextMenu={(event) => {
-                  event.preventDefault();
-                  handleDeleteElement(element.id);
-                }}
-                onDoubleClick={(event) => {
-                  event.preventDefault();
-                  handleEditElement(element.id)
-                }}
-                src={element.url}
-                style={{
-                  position: 'absolute',
-                  left: `${element.position.x}%`,
-                  top: `${element.position.y}%`,
-                  width: `${element.width}%`,
-                  height: `${element.height}%`,
-                  zIndex: index,
-                }}
-                autoPlay={element.autoPlay}
-                muted
-                loop
-              />
-            );
-          case 'code':
-            return (
-              <div
-                key={index}
-                onContextMenu={(event) => {
-                  event.preventDefault();
-                  handleDeleteElement(element.id);
-                }}
-                onDoubleClick={(event) => {
-                  event.preventDefault();
-                  handleEditElement(element.id)
-                }}
-                style={{
-                  position: 'absolute',
-                  left: `${element.position.x}%`,
-                  top: `${element.position.y}%`,
-                  width: `${element.width}%`,
-                  height: `${element.height}%`,
-                  zIndex: index,
-                }}
-              >
-                <SyntaxHighlighter language={element.language} style={materialLight}
-                                   customStyle={{ fontSize: `${element.fontSize}em` }}>
-                  {element.code}
-                </SyntaxHighlighter>
-              </div>
-            );
-          default:
-            return null;
-        }
+        return (
+          <ResizableBox
+            key={element.id} // Assuming each element has a unique id
+            element={element}
+            index={index}
+            handleDeleteElement={handleDeleteElement}
+            handleEditElement={handleEditElement}
+            handleUpdateElement={handleUpdateElement}
+            containerHeight={parseInt('1000px')}
+            containerWidth={parseInt('1600px')}
+          />
+        );
       })}
       <ElementModal
         type={dataType}
